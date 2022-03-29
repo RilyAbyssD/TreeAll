375ef0de70199e20d4702274733810b05d87ccd9
RilyAbyssD <hy200611304@gmail.com>
2022-03-29 10:42:05 +0900

    座標完成

--- src/main/java/rilyabyss/treeall/Listener/BlockBreakListener.java
+++ src/main/java/rilyabyss/treeall/Listener/BlockBreakListener.java
@@ -3,14 +3,10 @@ package rilyabyss.treeall.Listener;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.inventory.ItemStack;
-
-import java.util.ArrayList;
 
 public class BlockBreakListener implements Listener {
 
@@ -30,27 +26,70 @@ public class BlockBreakListener implements Listener {
         // 壊したブロックの最高の高さを取得
         int block = loc.getWorld().getHighestBlockYAt(loc.getBlockX(), loc.getBlockZ());
 
-        if (isAxe(p.getItemInHand().getType())) {
-            if (loc.getBlockY() != block) {
+        if (isAxe(p.getItemInHand().getType()))
+            if (loc.getBlockY() != block)
                 breakTree(loc, block);
-            } else {
-            }
-        } else {
-        }
-
-
     }
 
     public void breakTree(Location loc, int HighestBlock) {
 
         for (int i = loc.getBlockY(); i < HighestBlock; i++) {
+
             loc = loc.add(0, 1, 0);
 
+            // ブロックが木ではなかったら
+            if (!isTree(loc.getBlock().getType()))
+                break;
+            loc.getBlock().breakNaturally();
+
+            treeXPos(loc);
+        }
+
+    }
+
+    public void treeXPos(Location loc) {
+
+        // X座標
+        for (int x = 1; x < 4; x++) {
+            for (int xl = 1; xl < 4; xl++) {
+                // Z座標
+                for (int z = 1; z < 4; z++) {
+                    for (int lz = 1; lz < 4; lz++) {
+                        loc = loc.add(0, 0, -lz);
+                        // ブロックが木ではなかったら
+                        if (!isTree(loc.getBlock().getType())) {
+                            loc = loc.add(0, 0, lz);
+                            break;
+                        }
+                        loc.getBlock().breakNaturally();
+                        loc = loc.add(0, 0, lz);
+                    }
+                    loc = loc.add(0, 0, z);
+                    // ブロックが木ではなかったら
+                    if (!isTree(loc.getBlock().getType())) {
+                        loc = loc.add(0, 0, -z);
+                        break;
+                    }
+                    loc.getBlock().breakNaturally();
+                    loc = loc.add(0, 0, -z);
+                }
+                loc = loc.add(-xl, 0, 0);
+                // ブロックが木ではなかったら
+                if (!isTree(loc.getBlock().getType())) {
+                    loc = loc.add(xl, 0, 0);
+                    break;
+                }
+                loc.getBlock().breakNaturally();
+                loc = loc.add(xl, 0, 0);
+            }
+            loc = loc.add(x, 0, 0);
+            // ブロックが木ではなかったら
             if (!isTree(loc.getBlock().getType())) {
+                loc = loc.add(-x, 0, 0);
                 break;
             }
             loc.getBlock().breakNaturally();
-
+            loc = loc.add(-x, 0, 0);
         }
 
     }
